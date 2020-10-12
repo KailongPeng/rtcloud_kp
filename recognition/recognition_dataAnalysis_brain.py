b714c3fa48f0032bff20deecb9f1a8bd94547c60
KailongPeng <kailongpeng001@gmail.com>
2020-10-11 21:41:07 -0400

    reorganize

--- recognition/recognition_dataAnalysis_brain.py
+++
@@ -1,9 +0,0 @@
-# fetch data from XNAT
-
-
-# convert functional run to anatomical space
-
-anatomical=
-functional=
-
-# save all the function run data in anatomical space as ab M x N matrix. M TR, N voxels

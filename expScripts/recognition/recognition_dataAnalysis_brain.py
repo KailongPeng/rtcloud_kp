864fb1194c4c280fe164d9ef63e6b68846bb7f29
KailongPeng <kailongpeng001@gmail.com>
2020-10-11 21:44:16 -0400

    reorganize

---
+++ expScripts/recognition/recognition_dataAnalysis_brain.py
@@ -0,0 +1,105 @@
+# This script is in the environment of '/gpfs/milgram/project/turk-browne/projects/rtcloud_kp/' and activate_rt
+
+# import and set up environment
+from subprocess import call
+import nibabel as nib
+from nibabel.nicom import dicomreaders
+import pydicom as dicom  # type: ignore
+import numpy as np
+import time
+import os
+import glob
+import shutil
+homeDir='/gpfs/milgram/project/turk-browne/projects/rtcloud_kp/'
+dataDir=homeDir+'recognition/dataAnalysis/rtSynth_pilot001/nifti/'
+tmp_folder = '/gpfs/milgram/scratch60/turk-browne/kp578/sandbox/' # tmp_folder='/tmp/kp578/'
+# if os.path.isdir(tmp_folder):
+# 	shutil.rmtree(tmp_folder)
+
+
+os.mkdir(tmp_folder)
+
+def printOrien(full_ref_BOLD):
+	ref_BOLD_obj = nib.load(full_ref_BOLD)
+	ref_bold_ornt = nib.aff2axcodes(ref_BOLD_obj.affine)
+	print('Ref BOLD orientation:') # day1functionalInAnatomicalSpace Ref BOLD orientation:('R', 'A', 'S') # What is the orientation of the realtime data? 
+	print(ref_bold_ornt)
+
+
+
+# # fetch data from XNAT
+subjectID="rtSynth_pilot001"
+# call(f"sbatch fetchXNAT.sh {subjectID}",shell=True) # fetch data from XNAT
+# call(f"upzip {subjectID}.zip")
+# call(f"sbatch change2nifti.sh {subjectID}",shell=True) # convert dicom to nifti files
+
+# convert functional run to anatomical space
+anatomical=dataDir+'rtSynth_pilot001_20201009165148_8.nii'
+functional=dataDir+'rtSynth_pilot001_20201009165148_13.nii'
+
+# Determine the orientation of ref BOLD
+ref_BOLD_obj = nib.load(anatomical)
+ref_bold_ornt = nib.aff2axcodes(ref_BOLD_obj.affine)
+print('Ref BOLD orientation:')
+print(ref_bold_ornt)
+
+# Generate the transformation needed
+target_orientation = nib.orientations.axcodes2ornt(ref_bold_ornt)
+dicom_orientation = nib.orientations.axcodes2ornt(('P', 'L', 'S'))
+ornt_transform = nib.orientations.ornt_transform(dicom_orientation,target_orientation)
+
+
+
+# copy the functional data to tmp folder
+os.mkdir(tmp_folder+subjectID)
+call(f"cp {functional} {tmp_folder}{subjectID}/",shell=True)
+
+# split functional data to multiple volumes
+call(f"fslsplit {tmp_folder}{subjectID}/{functional.split('/')[-1]}  {tmp_folder}{subjectID}/",shell=True) ## os.chdir(f"{tmp_folder}{subjectID}/")
+functionalFiles=glob.glob(f'{tmp_folder}{subjectID}/*.nii.gz')
+functionalFiles.sort()
+
+# select the middle volume as the template functional volume
+templateFunctionalVolume=functionalFiles[int(len(functionalFiles)/2)]
+
+# align the middle volume to the anatomical data and visually check the result # save the middle volume as day1functionalInAnatomicalSpace.nii.gz
+day1functionalInAnatomicalSpace_bet=dataDir+"day1functionalInAnatomicalSpace_bet.nii.gz"
+templateFunctionalVolume_bet=f"{templateFunctionalVolume[0:-7]}_bet.nii.gz"
+call(f"bet {templateFunctionalVolume} {templateFunctionalVolume_bet}",shell=True)
+anatomical_bet=f"{anatomical[0:-4]}_bet.nii.gz"
+call(f"bet {anatomical} {anatomical_bet}",shell=True)
+functional2anatomical=dataDir+'functional2anatomical'
+call(f"flirt -in {templateFunctionalVolume_bet} -ref {anatomical_bet} \
+	-omat {functional2anatomical}\
+	-out {day1functionalInAnatomicalSpace_bet}",shell=True)
+
+
+# output_image_correct = nib.orientations.apply_orientation(temp_data, ornt_transform)
+
+
+# transform templateFunctionalVolume with skull to anatomical space using saved transformation
+day1functionalInAnatomicalSpace=dataDir+"day1functionalInAnatomicalSpace.nii.gz"
+call(f"flirt -in {templateFunctionalVolume} -ref {anatomical} -out {day1functionalInAnatomicalSpace} \
+-init {functional2anatomical} -applyxfm",shell=True) # the result of this looks fine, although not perfect.
+
+# align every other functional volume with this day1functionalInAnatomicalSpace
+data=[]
+for curr_TR in functionalFiles:
+	TRinAnatomical=f"{curr_TR[0:-7]}_AnatomicalSpace.nii.gz"
+	command = f"3dvolreg -base {day1functionalInAnatomicalSpace}\
+	-prefix  {TRinAnatomical} \
+	-1Dfile {curr_TR[0:-7]}_motion.1D -1Dmatrix_save {curr_TR[0:-7]}_mat.1D {curr_TR}"
+	print(command)
+	A=time.time()
+	call(command,shell=True)
+	B=time.time()
+	print(f"{B-A}s passed")
+	TRinAnatomical = nib.load(TRinAnatomical)
+	TRinAnatomical = TRinAnatomical.get_data()
+	data.append(TRinAnatomical)
+
+# save all the functional data in day1 anatomical space, as M x N matrix. M TR, N voxels
+recognitionData=np.asarray(data)
+recognitionData=recognitionData.reshape(recognitionData.shape[0],-1)
+np.save(dataDir+'recognitionData',recognitionData)
+

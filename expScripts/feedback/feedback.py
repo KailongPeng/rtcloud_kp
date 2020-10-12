b5d46943dc2665448ae6581b71411275b5fb50b3
KailongPeng <kailongpeng001@gmail.com>
2020-10-12 01:08:40 -0400

    change the feedback.py aka the feedback display code to be updated every TR rather than before update every 4 TRs

--- expScripts/feedback/feedback.py
+++ expScripts/feedback/feedback.py
@@ -16,22 +16,32 @@ import time
 import re
 alpha = string.ascii_uppercase
 
+
+main_dir="../../"
+
+
 # startup parameters
-IDnum = 'test' #sys.argv[1]
-run = 1 #int(sys.argv[2])
+IDnum = 'pilot_sub001' #sys.argv[1] # this is the name of the subject
+sess = 2 # int(sys.argv[2]) which session is this in? by design this can be 2 3 4 
+run = 1 #int(sys.argv[3]) # which run is this in
 scanmode = 'Test'  # 'Scan' or 'Test' or None
 screenmode = False  # fullscr True or False
+
+
 gui = True if screenmode == False else False
-monitor_name = "testMonitor"
+monitor_name = "testMonitor" #might be scanner?
 scnWidth, scnHeight = monitors.Monitor(monitor_name).getSizePix()
 frameTolerance = 0.001  # how close to onset before 'same' frame
+TRduration=2.0
 
-tune=4 # tune controls how much to morph, tune can range from (1,6.15] when paremeterrange is np.arange(1,20)
-parameterRange=np.arange(1,20) #define the range for possible parameters for preloading images.
-TRduration=1.5
-step=3 #in simulation, how quickly the morph changes ramp up
+# similation specific
+step=3 #in simulation, how quickly the morph changes ramp up. Note this is only for simulation, has nothing to do with real experiment
 
-TrialNumber=3 #how many trials are required
+# trial_list designing parameters
+TR=2 # the length of a TR is 2s
+parameterRange=np.arange(1,20) #define the range for possible parameters for preloading images. Preloading images is to make the morphing smooth during feedback
+tune=4 # this parameter controls how much to morph (how strong the morphing is) (used in preloading function), tune can range from (1,6.15] when paremeterrange is np.arange(1,20)
+TrialNumber=4 #how many trials are required
 
 ## - design the trial list: the sequence of the different types of components: 
 ## - e.g: ITI + waiting for fMRI signal + feedback (receive model output from feedbackReceiver.py)
@@ -46,54 +56,53 @@ trial_list.append({'Trial':None,
                     'newWobble':None},
                     ignore_index=True)
 for currTrial in range(1,1+TrialNumber):
-    for i in range(2): #8TR=12s
+    for i in range(1): # should be 6TR=12s
         trial_list=trial_list.append({'Trial':currTrial,
                                     'time':curTime,
                                     'TR':curTR,
                                     'state':'ITI',
                                     'newWobble':0},
                                     ignore_index=True)
-        curTime=curTime+TRduration
+        curTime=curTime+TR
         curTR=curTR+1
-    for i in range(2): #4TR=6s
+    for i in range(1): # should be 3TR=6s
         trial_list=trial_list.append({'Trial':currTrial,
                                     'time':curTime,
                                     'TR':curTR,
                                     'state':'waiting',
                                     'newWobble':0},
                                     ignore_index=True)
-        curTime=curTime+TRduration
+        curTime=curTime+TR
         curTR=curTR+1
-    for i in range(2): #8TR=12s
-        newWobble=1
-        for j in range(4):
-            trial_list=trial_list.append({'Trial':currTrial,
-                                        'time':curTime,
-                                        'TR':curTR,
-                                        'state':'feedback',
-                                        'newWobble':newWobble},
-                                        ignore_index=True)
-            newWobble=0
-            curTime=curTime+TRduration
-            curTR=curTR+1
-for i in range(2): #8TR=12s
+    for i in range(5): #5TR=10s
+        trial_list=trial_list.append({'Trial':currTrial,
+                                    'time':curTime,
+                                    'TR':curTR,
+                                    'state':'feedback',
+                                    'newWobble':1},
+                                    ignore_index=True)
+        curTime=curTime+TR
+        curTR=curTR+1
+for i in range(1): # should be 6TR=12s
     trial_list=trial_list.append({'Trial':currTrial,
                                 'time':curTime,
                                 'TR':curTR,
                                 'state':'ITI',
                                 'newWobble':0},
                                 ignore_index=True)
-    curTime=curTime+TRduration
+    curTime=curTime+TR
     curTR=curTR+1
-# Everytime there is a 1 in newWhoble, start a new timer lasting for 
-# that long, and change the image source every eachTime interval.
+    
+trial_list
 
-parameters = np.arange(1,step*(sum((trial_list['newWobble']==1)*1)),step) #[1,2,3,4,5,6,7,8]
+
+
+# parameters = np.arange(1,step*(sum((trial_list['newWobble']==1)*1)),step) #[1,2,3,4,5,6,7,8]
 
 print('total trial number=',TrialNumber)
 # print('neighboring morph difference=',tune)
 print('preloaded parameter range=',parameterRange)
-print('used parameters=',parameters)
+# print('used parameters=',parameters)
 
 
 mywin = visual.Window(
@@ -102,6 +111,19 @@ mywin = visual.Window(
     monitor='testMonitor', color=[0,0,0], colorSpace='rgb', #color=[0,0,0]
     blendMode='avg', useFBO=True,
     units='height')
+
+
+def sample(L,num=10):
+    # This functional uniformly sample the list to be num points
+    # e.g, if L is 0-99, num is 10, newList would be [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+    # e.g, if L is 0-95, num is 10, newList would be [8, 18, 27, 37, 47, 56, 66, 75, 85, 95]
+    # e.g, if L is 0-5, num is 10, newList would be [0, 0, 0, 1, 2, 2, 3, 3, 4, 5]
+    sampleStep=len(L)/num 
+    newList=[]
+    for i in range(1,num):
+        newList.append(L[int(i*sampleStep-1)])
+    newList.append(L[-1])
+    return newList
 
 
 # preload image list for parameter from 1 to 10.
@@ -115,6 +137,7 @@ def preloadimages(parameterRange=np.arange(1,20),tune=1):
         images=[]
         print('maximum morph=',round((tune*currParameter*numberOfUpdates+2)/numberOfUpdates+1))
         for axis in ['bedTable', 'benchBed']:
+            tmp_images=[]
             for currImg in range(1,int(round(tune*currParameter*numberOfUpdates+2)),int((currParameter*numberOfUpdates+2)/numberOfUpdates)):
                 currMorph=100-round(currImg/numberOfUpdates+1) if axis=='benchBed' else round(currImg/numberOfUpdates+1)
                 if currMorph<1 or currMorph>99:
@@ -128,8 +151,10 @@ def preloadimages(parameterRange=np.arange(1,20),tune=1):
                                                 color=[1,1,1], colorSpace='rgb', opacity=1,
                                                 flipHoriz=False, flipVert=False,
                                                 texRes=128, interpolate=True, depth=-4.0)
-                images.append(currImage)
+                tmp_images.append(currImage)
                 last_image='carchair_exp_feedback/{}_{}_{}.png'.format(axis,currMorph,5)
+            images=images+sample(tmp_images)
+            tmp_images=[]
             for currImg in reversed(range(1,int(round(tune*currParameter*numberOfUpdates+1)),int((currParameter*numberOfUpdates+2)/numberOfUpdates))):
                 currMorph=100-round(currImg/numberOfUpdates+1) if axis=='benchBed' else round(currImg/numberOfUpdates+1)
                 curr_image='carchair_exp_feedback/{}_{}_{}.png'.format(axis,currMorph,5)
@@ -141,17 +166,19 @@ def preloadimages(parameterRange=np.arange(1,20),tune=1):
                                                 color=[1,1,1], colorSpace='rgb', opacity=1,
                                                 flipHoriz=False, flipVert=False,
                                                 texRes=128, interpolate=True, depth=-4.0)
-                images.append(currImage)
+                tmp_images.append(currImage)
                 last_image='carchair_exp_feedback/{}_{}_{}.png'.format(axis,currMorph,5)
+            images=images+sample(tmp_images)
         imageLists.update( {currParameter : images} )
     end = time.time()
     print("preload image duration=", end - start)
     return imageLists
+
 imageLists=preloadimages(parameterRange=parameterRange,tune=tune)
 
-
 # Open data file for eye tracking
-datadir = "./data/feedback/"
+# datadir = "./data/feedback/"
+datadir = main_dir + f"subjects/{IDnum}/ses{sess}_feedback/"
 
 maxTR=int(trial_list['TR'].iloc[-1])+6
 # Settings for MRI sequence
@@ -165,16 +192,14 @@ if not os.path.exists('./data/feedback/'):
 
 # check if data for this subject and run already exist, and raise an error if they do (prevent overwriting)
 newfile = datadir+"{}_{}.csv".format(str(IDnum), str(run))
-
+if os.path.exists(newfile):
+    raise Exception(f'{newfile} exists')
 # create empty dataframe to accumulate data
 data = pd.DataFrame(columns=['Sub', 'Run', 'TR', 'time'])
 
 # Create the fixation dot, and initialize as white fill.
 fix = visual.Circle(mywin, units='deg', radius=0.05, pos=(0, 0), fillColor='white',
                     lineColor='black', lineWidth=0.5, opacity=0.5, edges=128)
-
-
-
 
 # start global clock and fMRI pulses (start simulated or wait for real)
 print('Starting sub {} in run #{}'.format(IDnum, run))
@@ -200,29 +225,25 @@ backgroundImage = visual.ImageStim(
     color=[1,1,1], colorSpace='rgb', opacity=1,
     flipHoriz=False, flipVert=False,
     texRes=128, interpolate=True, depth=-4.0)
+
 # trialClock is reset in each trial to change image every TR (2s), time for each image is 2/numOfImages
 trialClock = core.Clock()
 
 # trialClock.add(10)  # initialize as a big enough number to avoid text being shown at the first time.
-
-# Trial=list(trial_list['Trial'])
-# time=list(trial_list['time'])
 TR=list(trial_list['TR'])
 states=list(trial_list['state'])
 newWobble=list(trial_list['newWobble'])
 
 # parameters=np.round(np.random.uniform(0,10,sum((trial_list['newWobble']==1)*1)))
 # parameters = np.arange(1,1+sum((trial_list['newWobble']==1)*1)) #[1,2,3,4,5,6,7,8]
-
 ParameterUpdateDuration=np.diff(np.where(trial_list['newWobble']==1))[0][0]*TRduration
 curr_parameter=0
 remainImageNumber=[]
+feedbackParameterFileName=main_dir+f"subjects/{IDnum}/ses{sess}_feedbackParameter/run_{run}.csv"
 
-# frameTime=[]
 # While the running clock is less than the total time, monitor for 5s, which is what the scanner sends for each TR
 while len(TR)>1: #globalClock.getTime() <= (MR_settings['volumes'] * MR_settings['TR']) + 3:
     trialTime = trialClock.getTime()
-    #frameTime.append(trialTime)
     keys = event.getKeys(["5"])  # check for triggers
     if '0' in keys: # whenever you want to quite, type 0
         mywin.close()
@@ -231,15 +252,12 @@ while len(TR)>1: #globalClock.getTime() <= (MR_settings['volumes'] * MR_settings
         TR.pop(0)
         states.pop(0)
         newWobble.pop(0)
-        #Trial.pop(0)
-        #time.pop(0)
-        #print('\n\n 5')
         print(states[0])
         if states[0] == 'feedback' and newWobble[0]==1:
-            # fetch parameter from preprocessing process on Milgram
-            # parameter=parameters[curr_parameter]
-            parameters=pd.read_csv('./parameterWriteFolder/1')
-            parameter = parameters['value'].iloc[-1]
+            # fetch parameter from preprocessing process on Milgram            
+            parameters=pd.read_csv(feedbackParameterFileName)
+            parameter=parameters['value'].iloc[curr_parameter]
+
             curr_parameter=curr_parameter+1
             # start new clock for current updating duration (the duration in which only a single parameter is used, which can be 1 TR or a few TRs, the begining of the updateDuration is indicated by the table['newWobble'])
             trialClock=core.Clock()
@@ -249,7 +267,7 @@ while len(TR)>1: #globalClock.getTime() <= (MR_settings['volumes'] * MR_settings
             # calculated how long each image should last.
             eachTime=ParameterUpdateDuration/len(imagePaths)
             # update the image
-#             image.image=imagePaths[0]
+            # image.image=imagePaths[0]
             image.setAutoDraw(False)
             imagePaths[0].setAutoDraw(True)
             # currImage*eachTime is used in the calculation of the start time of next image in the list.
@@ -269,7 +287,6 @@ while len(TR)>1: #globalClock.getTime() <= (MR_settings['volumes'] * MR_settings
             # # discard the first image since it has been used.
             # imagePaths.pop(0)
     if (states[0] == 'feedback') and (trialTime>currImage*eachTime):
-            # image.image=imagePaths[0]
             try: # sometimes the trialTime accidentally surpasses the maximum time, in this case just do nothing, pass
                 imagePaths[currImage-1].setAutoDraw(False)
                 imagePaths[currImage].setAutoDraw(True)
@@ -289,33 +306,51 @@ while len(TR)>1: #globalClock.getTime() <= (MR_settings['volumes'] * MR_settings
                     print('curr morph=',currMorphParameter)
                 oldMorphParameter=currMorphParameter
                 currImage=currImage+1        
-                # imagePaths.pop(0)
             except:
                 pass
     elif states[0] == 'ITI':
         backgroundImage.setAutoDraw(True)
         fix.draw()
-        
-# #         try:
-#         print('hiding this image=',imagePaths[currImage-1],end='\n\n')
-#         imagePaths[currImage-1].setAutoDraw(False)
-# #         except:
-# #             pass
-# #         image.setAutoDraw(False)
     elif states[0] == 'waiting':
-#         image.image='./carchair_exp_feedback/bedChair_1_5.png'
         backgroundImage.setAutoDraw(False)
         image.setAutoDraw(True)
-        # sys.stdout.flush()
     # refresh the screen
     mywin.flip()
-# print('frameTime=',frameTime)
+
 
 # write data out!
-# behavioral data
 data.to_csv(newfile)
-# eye-tracking data
 mywin.close()
 core.quit()
 
 
+# ##############################################################################
+# ##############################################################################
+# ##############################################################################
+# ####################### simmulated data forparameters#########################
+# ##############################################################################
+# ##############################################################################
+# ##############################################################################
+# parameters = pd.DataFrame(columns=['runId','trId','value','timestamp'])
+# main_dir="/Volumes/GoogleDrive/My Drive/Turk_Browne_Lab/rtcloud_kp/"
+# parameterWriteFolder=main_dir+'./subjects/pilot_sub001/ses1_feedbackParameter/'
+# if not os.path.isdir(parameterWriteFolder):
+#     os.mkdir(parameterWriteFolder)
+
+# for i in range(20):
+
+#     runId=1
+#     trId=int(np.random.uniform(1,20,1))
+#     value=int(np.random.uniform(1,20,1))
+#     timestamp=int(np.random.uniform(0,10,1))
+#     parameterFileName=f"{parameterWriteFolder}run_{runId}.csv"
+#     print("Dequeue run: {}, tr: {}, value: {}, timestamp: {}".
+#           format(runId,trId,value,timestamp))
+
+#     parameters = parameters.append({'runId':runId,
+#                         'trId':trId,
+#                         'value':value,
+#                         'timestamp':timestamp},
+#                         ignore_index=True)
+#     print('parameters=',parameters)
+#     parameters.to_csv(parameterFileName)
